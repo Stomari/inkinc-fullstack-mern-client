efc5cd893e9a4812703454380d1e9b0f9c3376b1
dgentil13 <ahoydanni3@gmail.com>
2019-07-31 20:51:18 -0300

    Login/ SignUp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import AuthService from './components/auth/auth-service';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Components
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = { loggedInUser: null };
+    this.service = new AuthService();
+  }
+
+  fetchUser(){
+    if( this.state.loggedInUser === null ){
+      this.service.loggedin()
+      .then(response =>{
+        this.setState({
+          loggedInUser:  response
+        }) 
+      })
+      .catch( err =>{
+        this.setState({
+          loggedInUser:  false
+        }) 
+      })
+    }
+  }
+
+  getTheUser(userObj){
+    this.setState({
+      loggedInUser: userObj
+    })
+  }
+
+  logoutUser = () =>{
+    this.service.logout()
+    .then(() => {
+      this.setState({ loggedInUser: null });
+      this.props.getUser(null);  
+    })
+  }
+
+  render() {
+    this.fetchUser()
+    console.log(this.state)
+    if(this.state.loggedInUser){
+      return <Redirect to={'/'}/>
+    } else {
+      return (
+        <div className="App">
+        <Switch>
+          <Route exact path="/signup" render={() => <Signup getUser={() => this.getTheUser()}/>} />
+          <Route exact path='/' render={() => <Login getUser={() => this.getTheUser()}/>} />
+        </Switch>
+      </div>
+      )
+    }
+  }
 }
 
 export default App;
--- src/components/auth/auth-service.js
+++ src/components/auth/auth-service.js
@@ -9,11 +9,31 @@ class AuthService {
     this.service = service;
   }
 
-  signup = (username, password) => {
+  signup(username, password){
     return this.service.post('/signup', {username, password})
     .then(response => response.data)
   }
 
+  loggedin(){
+    return this.service.get('/loggedin')
+    .then(response => response.data)
+  }
+  logout(){
+    return this.service.post('/logout', {})
+    .then(response => response.data)
+  }
 }
+
+// logoutUser = () =>{
+//   this.service.logout()
+//   .then(() => {
+//     this.setState({ loggedInUser: null });
+//     this.props.getUser(null);  
+//   })
+// }
+
+// <Link to='/'>
+// <button onClick={() => this.logoutUser()}>Logout</button>
+// </Link>
 
 export default AuthService;
