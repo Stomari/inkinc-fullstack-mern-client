eac52684004188a9dbedcecc653323d40e2d7d6d
dgentil13 <ahoydanni3@gmail.com>
2019-08-06 13:33:29 -0300

    front page and navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,8 +15,11 @@ class Home extends Component {
               <h1>INK.inc</h1>
             </div>
           </section>
+
           <section className='about-custom d-flex justify-content-center'>
+
             <div className="card-deck home-custom">
+              
               <div className="card d-flex align-items-center card-custom">
                    <img src="/images/dribbble-show-series-1-7-05.png" className="card-img-top home-img pt-4" alt="..."/>
                   <div className="card-body mt-4 text-center">
@@ -39,11 +42,26 @@ class Home extends Component {
                     <h5 className="card-title text-uppercase">Book Now?</h5>
                     <p className="card-text">This is a longer card with supporting text below as a natural </p>
                   </div>
-                </div>  
+                </div> 
+
             </div>
+
           </section>
+
           <footer className="footer">
-            <p> footer </p>
+            <div className="row footer-content">
+
+              <div className="icons col-lg-6">
+                <img src="images/instagram-brands.svg"/>
+                <img src="images/facebook-brands.svg"/>
+                <img src="images/twitter-brands.svg"/>
+              </div>
+
+              <div className="col-lg-6 d-flex justify-content-end">
+                <p className="text-footer"> © 2019 Ink, Inc.</p>
+              </div>
+
+            </div>
           </footer>
         </Fragment>
       )
--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -7,7 +7,6 @@ import Folder from './folder/Folder';
 import CreateFolder from './folder/CreateFolder';
 import FavoriteArtists from './favoriteArtist/FavoriteArtists';
 
-
 class Profile extends Component {
   constructor(props){
     super(props);
@@ -36,32 +35,25 @@ class Profile extends Component {
   render(){
     return(
       <Fragment>
-        <div className="container-fluid profile-custom ">
+        <div className="container-fluid profile-custom">
           <div className="row m-5">
-            <div className="col-lg-3 text-center profile-side-header">
+            
+            <div className="col-lg-3 text-center profile-side-header mb-5">
               <Header user={this.props.user}/>
               <FavoriteArtists id="artists" artistInfo={() => this.getInfo()} artists={this.state.favoriteArtists}/>
             </div>
-            <div className="col-lg-8 p-0 profile-side-main">
-              <div className="folder-header d-flex justify-content-center">
-                <p className="row text-uppercase">
-                 <h4>Folders</h4>
-                  <CreateFolder foldersInfo={() => this.getInfo()}/>
+
+            <div className="col-lg-9 p-0 profile-side-main pr-5">
+              <div className="d-flex justify-content-center">
+                <p className="row text-uppercase folder-header d-flex justify-content-center mb-5">
+                 <h4>Tattoos you liked</h4>
+                  {/* <CreateFolder foldersInfo={() => this.getInfo()}/> */}
                 </p>
               </div>
               <Folder id="folder" foldersInfo={() => this.getInfo()} folders={this.state.folders}/>
             </div>
 
           </div>
-        {/* <ul class="nav nav-tabs">
-                <li class="nav-item">
-                    <a data-toggle="tab" class="nav-link active">Folders</a>
-                </li>
-                <li class="nav-item">
-                    <a data-target="#artists" data-toggle="tab" class="nav-link">Artists</a>
-                </li>
-                
-            </ul> */}
         </div>
       </Fragment>
     )
